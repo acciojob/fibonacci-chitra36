dd5c36687a6caa0b52829a1d7325fc8fd7c7d957
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-04-10 15:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	 [a, b] = [0, 1];
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b]
+  }
+}
+
+// instantiate the fib generator
+const fib_gen = fibonacci();
+
+// get first 10 numbers
+for (let i = 0; i < 10; i++) {
+  console.log(fib_gen.next().value);
 }
 
 module.exports = fibonacci;
