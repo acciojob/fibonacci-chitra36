90f124605e76a1ddad446c5283466811f6e1c07a
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-04-13 12:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if(num <= 2){
+		return num-1;
+	}
+	
+	let n1 =0; // 1
+	let n2 =1; // 2
+	var next = 0; // 2
+	var number = parseInt(num);
+	for(i =3;i <=number ; i++){
+		next = n1+n2;
+		n1 = n2;
+		n2 = next;
+	
+	}
+	return next;
+	
 	 [a, b] = [0, 1];
   while (true) {
     yield a;
